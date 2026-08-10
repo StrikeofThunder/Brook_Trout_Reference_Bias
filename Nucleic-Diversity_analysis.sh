811524494e6561150a1afd9537b576ad791fb92f
StrikeofThunder <96802122+StrikeofThunder@users.noreply.github.com>
2026-08-10 10:51:29 -0600

    Add files via upload
    Calculation and Graphing of genome wide Nucleotide Diversity

---
+++ Nucleic-Diversity_analysis.sh
@@ -0,0 +1,2 @@
+vcftools --gzvcf Virginia_noScaffolds.vcf.gz --window-pi 10000 --window-pi-step 10000 --out virginia_pi
+vcftools --gzvcf Canada_noScaffolds.vcf.gz --window-pi 10000 --window-pi-step 10000 --out canada_pi
